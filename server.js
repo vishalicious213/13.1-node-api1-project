717f86043b7e59995d7d4fa32646a0cff959c97d
Vish Singh <vishalicious213@gmail.com>
2020-06-03 23:29:17 -0700

    express server set up

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-const express = require("express")  // import express
-const server = express()            // declare express server
-server.use(express.json())          // middleware to parse JSON
+const express = require("express"); // import express
+const server = express();           // declare express server
+server.use(express.json());         // middleware to parse JSON
 
+const hostname = '127.0.0.1';       // local server name
+const port = 3000;                  //  listening port
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+    res.send('Hello from Express');
+  });
+
+// watch for connections on the specified port, below
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
