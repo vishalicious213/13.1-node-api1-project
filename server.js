bb4e250732d529817d133daad91117a2e7f113cc
Vish Singh <vishalicious213@gmail.com>
2020-06-04 00:10:54 -0700

    users data successfully retrieved with GET to /users

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // import express
 const server = express();           // declare express server
 server.use(express.json());         // middleware to parse JSON
-
+const db = require("./db.js")
 const hostname = '127.0.0.1';       // local server name
 const port = 3000;                  //  listening port
 
@@ -9,6 +9,12 @@ const port = 3000;                  //  listening port
 server.get('/', (req, res) => {
     res.send('Hello from Express');
   });
+
+  server.get("/users", (req, res) => {
+    const users = db.getUsers()
+    res.json(users)
+    // res.json({ message: "Hello, World!"})
+})
 
 // watch for connections on the specified port, below
 server.listen(port, hostname, () => {
