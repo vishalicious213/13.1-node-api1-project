5423e2973dcc663757d845802cbdc46c76d4d16a
Vish Singh <vishalicious213@gmail.com>
2020-06-07 13:17:44 -0700

    ShowUsers maps through userlist and renders it onscreen

--- client/src/components/ShowUsers.js
+++ client/src/components/ShowUsers.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ShowUsers = () => {
+    const [users, setUsers] = useState();
+
+    const getUsers = () => {
+        axios
+            .get('http://localhost:3001/users')
+            .then(response => {
+                console.log(response.data);
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.log("Error retrieving data: ", error);
+            })
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     return (
-        <div>ShowUsers</div>
+        <section>
+            {(users ?
+                users.map(user => (
+                    <div key={user.id}>
+                        <h4>{user.name}</h4>
+                        <p>{user.bio}</p>
+                    </div>
+                )) : null)}
+        </section>
     )
 }
 
